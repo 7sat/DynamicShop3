a253da74c8ea541ecf1b1a62185ab3d8322db24e
ahdg <36436808+ahdg6@users.noreply.github.com>
2020-12-14 23:06:06 +0800

    Added the Simplified Chinese Translation
    There were a lot of fallacies in the previous translator's translation, which I corrected and submitted as a built-in language file.

--- src/main/java/me/sat7/dynamicshop/utilities/LangUtil.java
+++ src/main/java/me/sat7/dynamicshop/utilities/LangUtil.java
@@ -224,7 +224,7 @@ public final class LangUtil {
             ccLang.save();
         }
 
-        // 영어
+        // 영어 English
         {
             ccLang.setup("Lang_v2_en-US",null);
             ccLang.get().addDefault("STARTPAGE.EDITOR_TITLE", "§3Start page editor");
@@ -434,9 +434,219 @@ public final class LangUtil {
             ccLang.save();
         }
 
+        // 简体中文 Simplifed Chinese
+        {
+            ccLang.setup("Lang_v2_zh-CN",null);
+            ccLang.get().addDefault("STARTPAGE.EDITOR_TITLE", "§3开始页面管理编辑");
+            ccLang.get().addDefault("STARTPAGE.EDIT_NAME", "更改名称");
+            ccLang.get().addDefault("STARTPAGE.EDIT_LORE", "更改简介");
+            ccLang.get().addDefault("STARTPAGE.EDIT_ICON", "更改图标");
+            ccLang.get().addDefault("STARTPAGE.EDIT_ACTION", "更改响应行为");
+            ccLang.get().addDefault("STARTPAGE.SHOP_SHORTCUT", "为商店创建快捷打开按钮");
+            ccLang.get().addDefault("STARTPAGE.CREATE_DECO", "创建装饰性按钮");
+            ccLang.get().addDefault("STARTPAGE.ENTER_SHOPNAME", "请输入商店名称。");
+            ccLang.get().addDefault("STARTPAGE.ENTER_NAME", "请输入新的名称");
+            ccLang.get().addDefault("STARTPAGE.ENTER_LORE", "请输入新的简介");
+            ccLang.get().addDefault("STARTPAGE.ENTER_ICON", "请输入新的物品图标名称 (Minecraft 材料名称。不区分大小写)");
+            ccLang.get().addDefault("STARTPAGE.ENTER_ACTION", "请输入一个不带 '/' 的命令。该命令将在点击按钮时被执行。");
+            ccLang.get().addDefault("STARTPAGE.ENTER_COLOR", "请输入一个颜色。(示例LIGHT_BLUE)");
+            ccLang.get().addDefault("STARTPAGE.DEFAULT_SHOP_LORE", "§f前往商店页面");
+
+            ccLang.get().addDefault("TRADE_TITLE", "§3贸易市场");
+            ccLang.get().addDefault("PALETTE_TITLE", "§3物品浏览");
+            ccLang.get().addDefault("PALETTE_LORE", "§e左击: 在商店中上架该物品");
+            ccLang.get().addDefault("ITEM_SETTING_TITLE", "§3物品设置");
+            ccLang.get().addDefault("QUICKSELL_TITLE", "§3一键出售");
+            ccLang.get().addDefault("TRADE_LORE", "§f点击: 前往贸易市场");
+            ccLang.get().addDefault("BUY", "§c购买");
+            ccLang.get().addDefault("BUYONLY_LORE", "§f该物品被设为 只可购买");
+            ccLang.get().addDefault("SELL", "§2Sell");
+            ccLang.get().addDefault("SELLONLY_LORE", "§f该物品被设为 只可收购");
+            ccLang.get().addDefault("PRICE", "§f购入: ");
+            ccLang.get().addDefault("SELLPRICE", "§f售出: ");
+            ccLang.get().addDefault("VALUE_BUY", "§f价值(买): ");
+            ccLang.get().addDefault("VALUE_SELL", "§f价值(卖): ");
+            ccLang.get().addDefault("PRICE_MIN", "§f最低价格: ");
+            ccLang.get().addDefault("PRICE_MAX", "§f最高价格: ");
+            ccLang.get().addDefault("MEDIAN", "§f中间值: ");
+            ccLang.get().addDefault("MEDIAN_HELP", "§f中间值越大，商品价格波动更敏感");
+            ccLang.get().addDefault("STOCK", "§f库存: ");
+            ccLang.get().addDefault("INFSTOCK", "无限库存");
+            ccLang.get().addDefault("STATICPRICE", "静态价格");
+            ccLang.get().addDefault("UNLIMITED", "无限制");
+            ccLang.get().addDefault("TAXIGNORED", "售出税将被忽略。");
+            ccLang.get().addDefault("TOGGLE_SELLABLE", "§e点击: 切换是否可卖");
+            ccLang.get().addDefault("TOGGLE_BUYABLE", "§e点击: 切换是否可买");
+            ccLang.get().addDefault("BALANCE", "§3余额");
+            ccLang.get().addDefault("ITEM_MOVE_LORE", "§e右击: 移动");
+            ccLang.get().addDefault("ITEM_COPY_LORE", "§e右击: 复制");
+            ccLang.get().addDefault("ITEM_EDIT_LORE", "§eShift+右击: 编辑");
+            ccLang.get().addDefault("DECO_CREATE_LORE", "§e右击: 添加装饰性按钮");
+            ccLang.get().addDefault("DECO_DELETE_LORE", "§eShift + 右击: 删除");
+            ccLang.get().addDefault("RECOMMEND", "§f使用系统推荐的价值");
+            ccLang.get().addDefault("RECOMMEND_LORE", "§f自动设置价值");
+            ccLang.get().addDefault("DONE", "§f完成操作");
+            ccLang.get().addDefault("DONE_LORE", "§f大功告成!");
+            ccLang.get().addDefault("ROUNDDOWN", "§f下调");
+            ccLang.get().addDefault("SETTOMEDIAN", "§f设为中间值");
+            ccLang.get().addDefault("SETTOSTOCK", "§f设为默认库存量");
+            ccLang.get().addDefault("SETTOVALUE", "§f设为价值");
+            ccLang.get().addDefault("SAVE_CLOSE", "§f保存并关闭");
+            ccLang.get().addDefault("CLOSE", "§f关闭");
+            ccLang.get().addDefault("CLOSE_LORE", "§f关闭这个窗口。");
+            ccLang.get().addDefault("REMOVE", "§f移除");
+            ccLang.get().addDefault("REMOVE_LORE", "§f从商店中移除这个物品。");
+            ccLang.get().addDefault("PAGE", "§f页码");
+            ccLang.get().addDefault("PAGE_LORE", "§f左击: 上一页 / 右击: 下一页");
+            ccLang.get().addDefault("PAGE_INSERT", "§eShift+左击: 插入页面");
+            ccLang.get().addDefault("PAGE_DELETE", "§eShift+右击: §c删除页面");
+            ccLang.get().addDefault("ITEM_MOVE_SELECTED", "已选择物品。在有空位的地方右击移动。");
+            ccLang.get().addDefault("SEARCH", "§f搜索");
+            ccLang.get().addDefault("SEARCH_ITEM", "§f请输入您正在寻找的物品的名字。");
+            ccLang.get().addDefault("SEARCH_CANCELED", "§f已取消搜索");
+            ccLang.get().addDefault("INPUT_CANCELED", "§f已取消数据键入");
+            ccLang.get().addDefault("ADDALL", "§f添加全部");
+            ccLang.get().addDefault("RUSURE", "§f您真的确定要那么做?请输入 'delete' 以二次确认。");
+            ccLang.get().addDefault("CANT_DELETE_LAST_PAGE", "§f这是最后一页了，您不能删除。");
+            ccLang.get().addDefault("SHOP_BAL_INF", "§f无限的货币");
+            ccLang.get().addDefault("SHOP_BAL", "§f商店货币总额");
+            ccLang.get().addDefault("SHOP_BAL_LOW", "§f商店没有足够的钱来维持营运了。");
+            ccLang.get().addDefault("ON","开启");
+            ccLang.get().addDefault("OFF","关闭");
+            ccLang.get().addDefault("SET","设置");
+            ccLang.get().addDefault("UNSET","撤销设置");
+            ccLang.get().addDefault("NULL(OPEN)","无 (为每个人都打开)");
+
+            ccLang.get().addDefault("LOG.LOG", "§f日志");
+            ccLang.get().addDefault("LOG.CLEAR", "§f已删除日志");
+            ccLang.get().addDefault("LOG.SAVE", "§f已保存日志");
+            ccLang.get().addDefault("LOG.DELETE", "§f删除日志");
+
+            ccLang.get().addDefault("SHOP_SETTING_TITLE", "§3商店设置");
+            ccLang.get().addDefault("SHOP_INFO", "§3商店信息");
+            ccLang.get().addDefault("PERMISSION", "§f所需权限");
+            ccLang.get().addDefault("CUR_STATE", "当前状态");
+            ccLang.get().addDefault("CLICK", "点击");
+            ccLang.get().addDefault("MAXPAGE", "§f最大页数");
+            ccLang.get().addDefault("MAXPAGE_LORE", "§f设置商店最大页数");
+            ccLang.get().addDefault("L_R_SHIFT", "§e左击: -1 右击: +1 Shift: x5");
+            ccLang.get().addDefault("FLAG", "§f配置节点");
+            ccLang.get().addDefault("RMB_EDIT", "§e右击: 编辑");
+            ccLang.get().addDefault("SIGNSHOP_LORE", "§f这家商店只能从告示牌处进入。");
+            ccLang.get().addDefault("LOCALSHOP_LORE", "§f玩家必须位于商店所在的实际地点。");
+            ccLang.get().addDefault("LOCALSHOP_LORE2", "§f这个配置节点需要一个位置才能生效。");
+            ccLang.get().addDefault("DELIVERYCHARG_LORE", "§f当您从远处买东西，您需要支付货运费。");
+            ccLang.get().addDefault("JOBPOINT_LORE", "§fJobs Reborn 点数商店。");
+
+            ccLang.get().addDefault("SHOP_CREATED", "§f已创建商店!");
+            ccLang.get().addDefault("SHOP_DELETED", "§f已删除商店!");
+            ccLang.get().addDefault("POSITION", "§f商店所处位置: ");
+            ccLang.get().addDefault("SHOP_LIST", "§f商店列表");
+
+            ccLang.get().addDefault("TIME.OPEN", "开市");
+            ccLang.get().addDefault("TIME.CLOSE", "闭市");
+            ccLang.get().addDefault("TIME.OPEN_LORE", "§f设置开市时间");
+            ccLang.get().addDefault("TIME.CLOSE_LORE", "§f设置闭市时间");
+            ccLang.get().addDefault("TIME.SHOPHOURS", "§f商店营业时间");
+            ccLang.get().addDefault("TIME.OPEN24", "24小时无休营业");
+            ccLang.get().addDefault("TIME.SHOP_IS_CLOSED", "§f商店已关门。开市时间: {time}h。当前时间: {curTime}时");
+            ccLang.get().addDefault("TIME.SET_SHOPHOURS", "设置商店营业时间");
+            ccLang.get().addDefault("TIME.CUR", "§f当前时间: {time}时");
+
+            ccLang.get().addDefault("FLUC.FLUCTUATION", "随机库存波动");
+            ccLang.get().addDefault("FLUC.INTERVAL", "波动周期");
+            ccLang.get().addDefault("FLUC.STRENGTH", "波动强度");
+
+            ccLang.get().addDefault("STOCKSTABILIZING.SS", "§f库存保持稳定");
+            ccLang.get().addDefault("STOCKSTABILIZING.L_R_SHIFT", "§e左击: -0.1 右击: +0.1 Shift: x5");
+
+            ccLang.get().addDefault("TAX.SALESTAX", "§f销售税");
+            ccLang.get().addDefault("TAX.USE_GLOBAL", "使用全局税率 ({tax}%)");
+            ccLang.get().addDefault("TAX.USE_LOCAL", "单独设置税率");
+
+            ccLang.get().addDefault("OUT_OF_STOCK", "§f库存告急!");
+            ccLang.get().addDefault("BUY_SUCCESS", "§f以 ${price} 的价格购买了 {item} x{amount}。余额: ${bal}");
+            ccLang.get().addDefault("SELL_SUCCESS", "§f以 ${price} 的价格出售了 {item} x{amount}。余额: ${bal}");
+            ccLang.get().addDefault("BUY_SUCCESS_JP", "§f以 ${price} 的工作点数购买了 {item} x{amount}。 余额: ${bal}");
+            ccLang.get().addDefault("SELL_SUCCESS_JP", "§f以 ${price} 的工作点数出售了 {item} x{amount}。 余额: ${bal}");
+            ccLang.get().addDefault("QSELL_RESULT", "§f给予: ");
+            ccLang.get().addDefault("QSELL_NA", "§f目前没有商店收购那件物品。");
+            ccLang.get().addDefault("DELIVERYCHARGE", "§f货运费");
+            ccLang.get().addDefault("DELIVERYCHARGE_EXEMPTION", "§f货运费: {fee} ({fee2} 免税)");
+            ccLang.get().addDefault("DELIVERYCHARGE_NA", "§f无法配送至不同的世界。");
+            ccLang.get().addDefault("NOT_ENOUGH_MONEY", "§f没有足够的钱。余额: {bal}");
+            ccLang.get().addDefault("NOT_ENOUGH_POINT", "§f没有足够的点数。余额: {bal}");
+            ccLang.get().addDefault("NO_ITEM_TO_SELL", "§f没有相应的物品可以售出。");
+            ccLang.get().addDefault("INVEN_FULL", "§4Inventory is full!");
+            ccLang.get().addDefault("IRREVERSIBLE", "§f这个操作将是不可逆的!");
+
+            ccLang.get().addDefault("HELP.TITLE", "§f帮助: {command} --------------------");
+            ccLang.get().addDefault("HELP.SHOP", "打开商店GUI。");
+            ccLang.get().addDefault("HELP.CMD", "切换是否启用命令帮助。");
+            ccLang.get().addDefault("HELP.CREATESHOP", "创建新的商店。");
+            ccLang.get().addDefault("HELP.DELETESHOP", "删除已有商店。");
+            ccLang.get().addDefault("HELP.SETTAX", "设置销售税");
+            ccLang.get().addDefault("HELP.SETTAX_TEMP", "设置临时销售税");
+            ccLang.get().addDefault("HELP.SHOPADDHAND", "添加手中物品至商店。");
+            ccLang.get().addDefault("HELP.SHOPADDITEM", "添加物品至商店。");
+            ccLang.get().addDefault("HELP.SHOPEDIT", "编辑商店上架商品。");
+            ccLang.get().addDefault("HELP.PRICE", "§7价格计算公式: 中间值*价值/库存");
+            ccLang.get().addDefault("HELP.INF_STATIC", "§7中间值<0 == 静态价格     库存<0 == 无限库存");
+            ccLang.get().addDefault("HELP.EDITALL", "编辑所有商店物品。");
+            ccLang.get().addDefault("HELP.EDITALL2", "§c警告。这个操作没有完整性检测机制，请小心使用。");
+            ccLang.get().addDefault("HELP.RELOAD", "重载 YML.");
+            ccLang.get().addDefault("HELP.RELOADED", "插件已重新加载");
+            ccLang.get().addDefault("HELP.USAGE", "用法");
+            ccLang.get().addDefault("HELP.CREATESHOP2", "权限(您可以稍后更改此项。)\n   true: dshop.user.shop.shopname\n   false: 不需要任何权限(默认)\n   用户输入: 需要所键入的权限");
+            ccLang.get().addDefault("HELP.ITEM_ALREADY_EXIST", "§7§o{item} 已经在上架销售了。\n   {info}\n   价值将被更新。");
+            ccLang.get().addDefault("HELP.ITEM_INFO", "§7§o{item} 正在出售:\n   {info}");
+            ccLang.get().addDefault("HELP.REMOVE_ITEM", "§f§o请输入 0 来作为数据值以 §4移除§f 这个物品。");
+            ccLang.get().addDefault("HELP.QSELL", "§f快速出售");
+            ccLang.get().addDefault("HELP.DELETE_OLD_USER", "从 User.yml 中删除不活跃的用户数据。");
+            ccLang.get().addDefault("HELP.CONVERT", "从其他插件转换数据。");
+            ccLang.get().addDefault("HELP.ACCOUNT", "设置商店货币总额。 -1 = 无限");
+
+            ccLang.get().addDefault("ITEM_ADDED", "已添加物品!");
+            ccLang.get().addDefault("ITEM_UPDATED", "已更新物品!");
+            ccLang.get().addDefault("ITEM_DELETED", "已移除物品!");
+            ccLang.get().addDefault("CHANGES_APPLIED", "已应用更改。新的价值: ");
+            ccLang.get().addDefault("RECOMMAND_APPLIED", "已应用系统建议价值。基于 {playerNum}名玩家。该值能在配置文件中被编辑。");
+            ccLang.get().addDefault("TRANSFER_SUCCESS", "转移成功。");
+
+            ccLang.get().addDefault("ERR.NO_USER_ID", "§6无法在服务器中找到您的uuid。商店对您来说无法使用。");
+            ccLang.get().addDefault("ERR.ITEM_NOT_EXIST", "物品不存在。");
+            ccLang.get().addDefault("ERR.ITEM_FORBIDDEN", "物品已被禁止上架。");
+            ccLang.get().addDefault("ERR.NO_PERMISSION", "§e没有相应权限。");
+            ccLang.get().addDefault("ERR.WRONG_USAGE", "错误的用法");
+            ccLang.get().addDefault("ERR.NO_EMPTY_SLOT", "商店已满");
+            ccLang.get().addDefault("ERR.WRONG_DATATYPE", "错误的参数类型。");
+            ccLang.get().addDefault("ERR.VALUE_ZERO", "该参数必须大于 0");
+            ccLang.get().addDefault("ERR.WRONG_ITEMNAME", "没有这样的物品。");
+            ccLang.get().addDefault("ERR.HAND_EMPTY", "您必须手持代售物品才能进行出售操作。");
+            ccLang.get().addDefault("ERR.HAND_EMPTY2", "§c§o您必须手持待售物品才能进行出售!");
+            ccLang.get().addDefault("ERR.SHOP_NOT_FOUND", "§f未找到商店");
+            ccLang.get().addDefault("ERR.SHOP_EXIST", "这个名称已然存在。");
+            ccLang.get().addDefault("ERR.SIGNSHOP_REMOTE_ACCESS", "您不能远程打开一个告示牌商店。");
+            ccLang.get().addDefault("ERR.LOCALSHOP_REMOTE_ACCESS", "您不能远程打开一个区位商店。");
+            ccLang.get().addDefault("ERR.MAX_LOWER_THAN_MIN", "最大价格必须高于最低价格");
+            ccLang.get().addDefault("ERR.DEFAULT_VALUE_OUT_OF_RANGE", "价格必须处于价值所规定的区间");
+            ccLang.get().addDefault("ERR.NO_RECOMMAND_DATA", "在 Worth.yml 没有找到可用的数据。");
+            ccLang.get().addDefault("ERR.JOBSREBORN_NOT_FOUND", "'Jobs Reborn' 未被找到。");
+            ccLang.get().addDefault("ERR.SHOP_HAS_INF_BAL", "{shop} 拥有无限的货币。");
+            ccLang.get().addDefault("ERR.SHOP_DIFF_CURRENCY", "这些商店使用着不同的货币。");
+            ccLang.get().addDefault("ERR.PLAYER_NOT_EXIST", "玩家不存在。");
+            ccLang.get().addDefault("ERR.SHOP_LINK_FAIL", "他们之中必须有一个实体账户。");
+            ccLang.get().addDefault("ERR.SHOP_LINK_TARGET_ERR", "目标商店必须有一个实体账户。");
+            ccLang.get().addDefault("ERR.NESTED_STRUCTURE", "镶套结构是不被允许的。(例如. aa-bb, bb-cc)");
+            ccLang.get().addDefault("ERR.CREATIVE","§e由于没有权限，您无法在创造模式下使用这个命令。");
+
+            ccLang.get().options().copyDefaults(true);
+            ccLang.save();
+        }
+
         if(lang == null) lang = "en-US";
 
-        if(!lang.equals("en-US") && !lang.equals("ko-KR"))
+        if(!lang.equals("en-US") && !lang.equals("ko-KR") && !lang.equals("zh-CN"))
         {
             ConfigurationSection conf = ccLang.get().getConfigurationSection("");
 
